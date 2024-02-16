b45d82de260b64038d7d9c3596d9f6f2a58273a6
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-16 20:55:05 +0200

    chore amtVisitors

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,16 +1,42 @@
 import { AnalyticsDashboard } from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = async ({}) => {
-  const pageview = await analytics.retrieveDays("pageview", 1);
+  const TRACKING_DAYS = 7;
+
+  const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
+
+  const totalPageviews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1);
+
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+      );
+    }, 0);
 
   return (
     <div className="min-h-screen w-full py-12 flex justify-center items-center">
       <div className="relative w-full max-w-6xl mx-auto text-white">
-        <AnalyticsDashboard />
+        <AnalyticsDashboard
+          avgVisitorsPerDay={avgVisitorsPerDay}
+          amtVisitorsToday={amtVisitorsToday}
+        />
       </div>
     </div>
   );
--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,12 +1,35 @@
+"use client";
+import { Card } from "@tremor/react";
 import { FC } from "react";
 
-interface AnalyticsDashboardProps {}
+interface AnalyticsDashboardProps {
+  avgVisitorsPerDay: string;
+  amtVisitorsToday: number;
+}
 
-export const AnalyticsDashboard: FC<AnalyticsDashboardProps> = ({}) => {
+export const AnalyticsDashboard: FC<AnalyticsDashboardProps> = ({
+  avgVisitorsPerDay,
+  amtVisitorsToday,
+}) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
-        
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Avg. visitors/day
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {avgVisitorsPerDay}
+          </p>
+        </Card>
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Visitors today
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {amtVisitorsToday}
+          </p>
+        </Card>
       </div>
     </div>
   );
